b3aadbdca9b58d5eb49f5414a29aa83097d481e6
indonswe <afc2002afc@hotmail.com>
2021-05-01 11:19:49 +0200

    Adding entity Annons and Category

---
+++ src/main/java/com/example/springbootformexample/model/Annons.java
@@ -0,0 +1,69 @@
+package com.example.springbootformexample.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Annons {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private int size_of_area;
+    private int size_of_buildings;
+    private int price;
+    private Category category;
+
+    public Annons(String name, int size_of_area, int size_of_buildings, int price, Category category) {
+        this.name = name;
+        this.size_of_area = size_of_area;
+        this.size_of_buildings = size_of_buildings;
+        this.price = price;
+        this.category = category;
+    }
+    public Annons(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize_of_area() {
+        return size_of_area;
+    }
+
+    public void setSize_of_area(int size_of_area) {
+        this.size_of_area = size_of_area;
+    }
+
+    public int getSize_of_buildings() {
+        return size_of_buildings;
+    }
+
+    public void setSize_of_buildings(int size_of_buildings) {
+        this.size_of_buildings = size_of_buildings;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/example/springbootformexample/model/Category.java
@@ -0,0 +1,8 @@
+package com.example.springbootformexample.model;
+
+public enum Category {
+    House,
+    Forest,
+    Rubber_trees,
+    House_and_forest
+}

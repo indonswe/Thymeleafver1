a0e368339a21379ca525f2ebda6960f7915f6485
indonswe <afc2002afc@hotmail.com>
2021-05-03 20:04:13 +0200

    The past query error fixed

--- src/main/java/com/example/springbootformexample/controller/AddBlasterController.java
+++ src/main/java/com/example/springbootformexample/controller/AddBlasterController.java
@@ -1,8 +1,11 @@
 package com.example.springbootformexample.controller;
 
+import com.example.springbootformexample.data.AnnonsRepository;
 import com.example.springbootformexample.data.BlasterRepository;
+import com.example.springbootformexample.model.Annons;
 import com.example.springbootformexample.model.Blaster;
 import com.example.springbootformexample.model.BlasterAmmunitionQuality;
+import com.example.springbootformexample.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +14,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AddBlasterController {
-    private final BlasterRepository repository;
+    //private final BlasterRepository repository;
+    private final AnnonsRepository repository;
 
     @Autowired
-    public AddBlasterController(BlasterRepository repository) {
+    public AddBlasterController(AnnonsRepository repository) {
         this.repository = repository;
     }
+    /*public AddBlasterController(BlasterRepository repository) {
+        this.repository = repository;
+    }*/
 
     @GetMapping("/add-blaster")
     public String getAddBlasterPage() {
         return "add-blaster";
     }
 
+    private String name;
+    private String provins;
+    private String kommun;
+    private int size_of_area;
+    private int size_of_buildings;
+    private int price;
+    private Category category;
+
     @PostMapping("/blasters")
-    public String addBlaster(@RequestParam("name") String name,
+    public String addAd(@RequestParam("name") String name,
+    @RequestParam("provins") String provins,
+    @RequestParam("kommun") String kommun,
+    @RequestParam("size_of_area") int size_of_area,
+    @RequestParam("size_of_buildings") int size_of_buildings,
+    @RequestParam("price") int price,
+    @RequestParam("category") Category category){
+        repository.save(new Annons(name, provins,kommun,size_of_area,size_of_buildings,price,category));
+    /*public String addBlaster(@RequestParam("name") String name,
                              @RequestParam("capacity") int capacity,
                              @RequestParam("quality") BlasterAmmunitionQuality quality) {
-        repository.save(new Blaster(name, capacity, quality));
+        repository.save(new Annons(name, capacity, quality));*/
 
         return "redirect:/";
     }

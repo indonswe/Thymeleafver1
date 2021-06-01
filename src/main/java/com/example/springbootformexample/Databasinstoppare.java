96e6a1efdb4c1ef93b2310e3f67e33a41d2df30f
indonswe <afc2002afc@hotmail.com>
2021-06-01 13:04:33 +0200

    Trying to get image from mySql database

--- src/main/java/com/example/springbootformexample/Databasinstoppare.java
+++ src/main/java/com/example/springbootformexample/Databasinstoppare.java
@@ -33,4 +33,36 @@ public class Databasinstoppare implements ApplicationRunner {
             //repository.save(person);
         }
     }
+
+    /*This is how I DID it.
+I store my images as a LONGBLOB in my MySQL database so use columnDefinition = "LONGBLOB". In your HTML code for your upload button, make sure you include the multiple = "true" in your tag if you are using HTML.
+
+In your Controller class make a multipart file that will get the images if you are using binding models and also, you need to have a Set/List of images so you can go through them all.
+
+MultipartFile[] images = bindingModel.getImages();
+Set<Image> newImages = new HashSet<>();
+I have this neat for loop so I can go through all of my images:
+
+    for (MultipartFile multipartFileFOREACH : images)
+        {
+            Image newImage = new Image(multipartFileFOREACH.getBytes(), postEntity);//Image is an Entity here, postEntity is an object
+            newImages.add(newImage);
+            this.imageRepository.saveAndFlush(newImage);
+        }
+        this.articleRepository.saveAndFlush(articleEntity);
+So, here I store my bytes into the LONGBLOB in the SQL database
+
+Then for the actual view
+
+    Set<Image> imagesBytes = post.getImages();
+    List<String> images = new ArrayList<>();
+    for (Image image : imagesBytes)
+    {
+        images.add(Base64.getEncoder().encodeToString(image.getLink()));
+    }
+My Image Entity has a link variable of type byte[]
+
+Then I simply add the view for the HTML:
+
+model.addAttribute("images", images);*/
 }

6e88c3e89ab294ef5af4dcd03b3fc14d13dd874c
indonswe <afc2002afc@hotmail.com>
2021-06-04 10:43:37 +0200

    Trying to add choosing and sorting functionality to index page

--- src/main/java/com/example/springbootformexample/controller/IndexController.java
+++ src/main/java/com/example/springbootformexample/controller/IndexController.java
@@ -2,10 +2,13 @@ package com.example.springbootformexample.controller;
 
 import com.example.springbootformexample.data.AnnonsRepository;
 import com.example.springbootformexample.data.BlasterRepository;
+import com.example.springbootformexample.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -17,10 +20,26 @@ public class IndexController {
         this.repository = repository;
     }
 
+    @PostMapping("/query")
+
+    public String query(@RequestParam("category") Category category){
+
+        return "index";
+    }
+
+    @PostMapping("/sortby")
+
+    public String sort(@RequestParam("category") Category category){
+
+        return "index";
+    }
+
     @GetMapping("/")
     public String getIndexPage(Model model) {
         model.addAttribute("annonsDTOs", repository.findAllAsDTO());
         System.out.println(repository.findByPerson(1));
+
+
 
         return "index";
     }

b3130133e0af4737ca73b6f570d6dba3f8135685
indonswe <afc2002afc@hotmail.com>
2021-06-17 21:42:49 +0200

    bankaccount equal null

--- src/main/java/com/example/springbootformexample/controller/IndexController.java
+++ src/main/java/com/example/springbootformexample/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.springbootformexample.controller;
 
 import com.example.springbootformexample.data.AnnonsRepository;
 import com.example.springbootformexample.data.BlasterRepository;
+import com.example.springbootformexample.model.BankAccount;
 import com.example.springbootformexample.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
     //private final BlasterRepository repository;
     private final AnnonsRepository repository;
-    int sort = 1;
+    int sort = 0;
+    BankAccount account = new BankAccount(500, "Kalle", "Anka", 12345);
+    int account1;
 
     @Autowired
     public IndexController(AnnonsRepository repository) {
@@ -34,18 +37,38 @@ public class IndexController {
 
     public String sort(@RequestParam("category") Category category){
         if (category.equals("HOUSE")){
+            account1 = account.deposit(100);
+
             sort = 1;
         }
         else if (category.equals("FOREST")){
+            account1 = account.withdraw(100);
             sort = 2;
         }
+
+        account1 = account.deposit(100);
+
+        System.out.println(sort);
+
+        System.out.println(account.getBalanceAccount());
+
+
+
         return "index";
     }
 
     @GetMapping("/")
     public String getIndexPage(Model model) {
 
-        model.addAttribute("balanceAccount", new java.util.Date());
+        System.out.println(account.getBalanceAccount());
+
+        account1 = account.getBalanceAccount();
+
+        //account1 = 15;
+
+        model.addAttribute("balanceAccount", account1);
+
+        //model.addAttribute("balanceAccount", new java.util.Date());
 
         /*if (sort==1) {
            model.addAttribute("annonsDTOs", repository.findAllAsDTO());
